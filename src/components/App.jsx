cf2a9c5230f68121b7757196aae466234ba3e13b
NicoOO1223 <nicosw.biz@gmail.com>
2024-09-21 18:21:20 +0200

    establish basic layout

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import '../../public/styles.css';
+function App() {
+  return (
+   <div>
+        <header class="header">
+            <a href="#" class="logo">Logo</a>
+            <nav class="navbar">
+                <a href="#">Home</a>
+                <a href="#">Oferta</a>
+                <a href="#">Info</a>
+                <a href="#">Insta</a>
+            </nav>
+        </header>
+
+        <div class="conatiner">
+          <div class="welcome-view">
+
+          </div>
+          <div class="Oferta">
+
+          </div>
+          <div class="Info">
+
+          </div>
+          <div class="insta">
+
+          </div>
+          <footer>
+
+          </footer>
+        </div>
+    </div>
+  );
+}
+
+export default App;
