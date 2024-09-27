0bcbc59deafe6f59608e6a037e88267120147669
NicoOO1223 <nicosw.biz@gmail.com>
2024-09-27 18:42:29 +0200

    final comit?

---
+++ src/components/SomeSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SomeSection.css'
+
+function SomeSection() {
+    return (
+      <div className='filler'>
+        <h1>Tu tez cos bedzie</h1>  
+      </div>
+    );
+  }
+  
+  export default SomeSection;
+  
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -2,12 +2,16 @@ import React from 'react'
 import '../../App.css'
 import HeroSection from '../HeroSection'
 import Cards from '../Cards'
+import Footer from '../Footer'
+import SomeSection from '../SomeSection'
 
 function Home () {
     return (
         <>
             <HeroSection />
             <Cards />
+            <SomeSection />
+            <Footer />
         </>
     )
 }
