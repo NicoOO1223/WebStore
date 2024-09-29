dbaf67956e9ce2aac7d5c0951b1b561df37ebc30
Kondziosz <cielinskikonrad@gmail.com>
2024-09-29 15:07:34 +0200

    added  playing video on start of the page

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
+import './Video.css';
+import videoFile from '../Videos/vidproj.mp4'
 
 function Video()
 {
-    return (
-        <div >
-            <ReactPlayer
-                url='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-                controls
-                width='100%'
-                height='100%'
-            />
-        </div>)
+    return (<>
+        <div className='video-player'>
+            <div className='player-wrapper'>
+                <ReactPlayer
+                    url={ videoFile }
+                    playing={ true }
+                    muted={ true }
+                    loop={ true }
+                    controls
+                    className='react-player'
+                    onError={ (e) => console.error('Error loading video:', e) }
+                /></div>
+        </div></>)
 }
+
 export default Video;
